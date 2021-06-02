779907821be44875adeaf333b6ce8c4a522b4285
kupriyanovde <kupriyanovde@ya.ru>
2021-06-02 08:04:00 +0600

    update

--- run.py
+++ run.py
@@ -4,7 +4,7 @@
 
 if __name__ == '__main__':
     import os, sys
-    from app import Application
+    from src import Application
     
     app = Application(os.path.dirname(__file__), sys.argv)
     sys.exit(app.exec_())
--- src/__init__.py
+++ src/__init__.py
@@ -1,11 +1,20 @@
-from typing import List
 
-from consolemenu import *
-from consolemenu.items import *
+
+from typing import List
+import os
+
+from .ASID import Core
 
 class Application:
-    def __init__(self, app_dir : str, argv : List[str]) -> None:
-        pass
+    def __init__(self, app_path : str):
+        self._app_path = app_path
 
-    def exec_(self) -> int:
-        pass
+        plugin_dir = os.path.join(app_path,'plugins')
+        core = Core(plugin_dir)
+        
+    def run(self, argv):
+        app = App()
+        try:
+            app.cmdloop()
+        except KeyboardInterrupt:
+            print ("завершение сеанса...")
